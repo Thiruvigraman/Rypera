fcdab9a311090f4b3749f129087f5f1187d8f6d4
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2025-04-25 15:58:26 +0530

    Update discord_webhook.py

--- discord_webhook.py
+++ discord_webhook.py
@@ -1,15 +1,12 @@
 import requests
 import os
 
-def log_to_discord(webhook_url, message):
-    payload = {
-        "content": message
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
+def log_to_discord(webhook_url: str, message: str):
+    payload = {"content": message}
+    headers = {"Content-Type": "application/json"}
+    
     try:
         response = requests.post(webhook_url, json=payload, headers=headers)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        print(f"Error logging to Discord: {e}")
+        print(f"❌ Error logging to Discord: {e}")
