c1f3adedfece79fbd210b7e196ec2e29ba85b2c0
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2025-04-25 16:14:10 +0530

    Update discord_webhook.py
    new

--- discord_webhook.py
+++ discord_webhook.py
@@ -4,7 +4,7 @@ import os
 def log_to_discord(webhook_url: str, message: str):
     payload = {"content": message}
     headers = {"Content-Type": "application/json"}
-    
+
     try:
         response = requests.post(webhook_url, json=payload, headers=headers)
         response.raise_for_status()
