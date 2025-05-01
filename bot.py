b6c3672fa8c8e78f768d77a861e44594d9a462a6
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2025-05-01 17:19:20 +0530

    Created bot.py

---
+++ bot.py
@@ -0,0 +1,39 @@
+#bot.py
+import requests
+import threading
+from config import BOT_TOKEN, DISCORD_WEBHOOK_STATUS
+from utils import log_to_discord
+
+def send_message(chat_id, text, parse_mode=None):
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+    payload = {'chat_id': chat_id, 'text': text}
+    if parse_mode:
+        payload['parse_mode'] = parse_mode
+    response = requests.post(url, json=payload)
+    if not response.ok:
+        log_to_discord(DISCORD_WEBHOOK_STATUS, f"Error sending message: {response.text}")
+    return response.json()
+
+def send_file(chat_id, file_id):
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendDocument'
+    payload = {'chat_id': chat_id, 'document': file_id}
+    response = requests.post(url, json=payload)
+    message_data = response.json()
+
+    if message_data.get('ok'):
+        file_message_id = message_data['result']['message_id']
+        warning_text = (
+            "❗️ *IMPORTANT* ❗️\n\n"
+            "This Video / File Will Be Deleted In *30 minutes* _(Due To Copyright Issues)_\n\n"
+            "📌 *Please Forward This Video / File To Somewhere Else And Start Downloading There.*"
+        )
+        warning_response = send_message(chat_id, warning_text, parse_mode="Markdown")
+        warning_message_id = warning_response['result']['message_id']
+
+        threading.Timer(1800, delete_message, args=[chat_id, file_message_id]).start()
+        threading.Timer(1800, delete_message, args=[chat_id, warning_message_id]).start()
+
+def delete_message(chat_id, message_id):
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/deleteMessage'
+    payload = {'chat_id': chat_id, 'message_id': message_id}
+    requests.post(url, json=payload)
