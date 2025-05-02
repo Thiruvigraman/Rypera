cf6397b0472944285454de0e60866c319da67e45
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2025-05-02 15:20:02 +0530

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -8,6 +8,6 @@ threads = 2
 timeout = 30
 max_requests = 100
 max_requests_jitter = 10
-loglevel = "info"  # Changed from default (debug) to info
+loglevel = "info"  # Changed from debug to info
 accesslog = "-"
 errorlog = "-"
