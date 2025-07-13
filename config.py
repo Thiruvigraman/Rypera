08cf03585481d774c2bc9be6973d249d2009b079
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2025-07-13 21:56:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 # config.py
+
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# Configuration variables
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 ADMIN_ID = os.getenv('ADMIN_ID')
 BOT_USERNAME = os.getenv('BOT_USERNAME')
@@ -14,9 +13,7 @@ DISCORD_WEBHOOK_LIST_LOGS = os.getenv('DISCORD_WEBHOOK_LIST_LOGS')
 DISCORD_WEBHOOK_FILE_ACCESS = os.getenv('DISCORD_WEBHOOK_FILE_ACCESS')
 MONGODB_URI = os.getenv('MONGODB_URI')
 
-# Validate required variables
 if not BOT_TOKEN or not ADMIN_ID or not BOT_USERNAME or not MONGODB_URI:
     raise ValueError("Missing environment variables")
 
-# Convert ADMIN_ID to integer
 ADMIN_ID = int(ADMIN_ID)
