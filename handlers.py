4f1c33f77ef4a9431a04f119e64a35ecaf8d21b8
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2025-07-15 00:23:34 +0530

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -2,7 +2,7 @@
 
 from config import ADMIN_ID, BOT_USERNAME, DISCORD_WEBHOOK_LIST_LOGS, DISCORD_WEBHOOK_FILE_ACCESS
 from database import load_movies, save_movie, delete_movie, rename_movie, add_user, get_all_users, get_stats, db
-from telegram import send_message, send_file, send_announcement
+from telegram_bot import send_message, send_file, send_announcement
 from discord import log_to_discord
 import time
 import psutil
