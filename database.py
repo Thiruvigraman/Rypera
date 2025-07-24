46ddffa8b75d83971d1abae8982b698990949b38
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2025-07-24 09:37:04 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@
 
 from pymongo import MongoClient
 from config import MONGODB_URI, DISCORD_WEBHOOK_STATUS
-from webhook import log_to_discord  # Updated import from discord to webhook
+from webhook import log_to_discord
 
 # MongoDB Setup
 try:
