78357b016f3d9b098ee1a8359a452f3b9ffcb270
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-04-25 11:49:58 +0530

    Update database.py

--- database.py
+++ database.py
@@ -278,6 +278,38 @@ def delete_sent_file_record(chat_id, file_message_id):
         pass
 
 
+# ================= CACHE =================
+
+MOVIE_CACHE = {}
+LAST_CACHE_TIME = 0
+CACHE_TTL = 60  # seconds
+
+
+def load_movies_cached():
+    global MOVIE_CACHE, LAST_CACHE_TIME
+
+    if not MONGO_AVAILABLE:
+        return {}
+
+    now = time.time()
+
+    if now - LAST_CACHE_TIME > CACHE_TTL:
+        try:
+            MOVIE_CACHE = {
+                doc['name']: {
+                    "file_id": doc['file_id'],
+                    "token": doc.get("token")
+                }
+                for doc in movies_collection.find(
+                    {}, {"name": 1, "file_id": 1, "token": 1, "_id": 0}
+                )
+            }
+            LAST_CACHE_TIME = now
+        except Exception:
+            return {}
+
+    return MOVIE_CACHE
+
 # ================= DB SIZE =================
 def get_db_size_mb():
     if not MONGO_AVAILABLE:
