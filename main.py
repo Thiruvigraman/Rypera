dc210c163010fcfe86776893b83f7ce1388f9ac9
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-04-26 12:26:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -177,25 +177,33 @@ def start_background_monitor():
 
 
 
+
 def log_worker():
     while True:
         try:
             logs = get_unsent_logs(10)
 
             for log in logs:
-                log_to_discord(
-                    "🎬 File accessed",
-                    "access",
-                    "info",
-                    fields={
-                        "user_id": log["user_id"],
-                        "movie": log["movie"]
-                    }
-                )
+                success = False
 
-                mark_log_sent(log["_id"])
+                try:
+                    log_to_discord(
+                        "🎬 File accessed",
+                        "access",
+                        "info",
+                        fields={
+                            "user_id": log["user_id"],
+                            "movie": log["movie"]
+                        }
+                    )
+                    success = True
+                except:
+                    success = False
 
-                time.sleep(0.7)  # prevent rate limit
+                if success:
+                    mark_log_sent(log["_id"])
+
+                time.sleep(0.7)
 
         except Exception:
             pass
