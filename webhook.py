b408d9264f577388fe400de8eabecc6bc3a3d578
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-04-26 13:18:33 +0530

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -222,12 +222,10 @@ def log_to_discord(
 
         entry["fields"]["source"] = log_type
 
-        # ✅ prevent memory overflow
         if log_queue.qsize() > 10000:
             print("Queue overflow, dropping logs")
             return True
 
-        # push to queue
         log_queue.put({**entry, "log_type": log_type})
 
         return True
