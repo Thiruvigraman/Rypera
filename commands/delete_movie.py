cbbafde13d42e1e0151f18f2d0ff6f8e47ceb164
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-04-26 13:50:48 +0530

    Update delete_movie.py

--- commands/delete_movie.py
+++ commands/delete_movie.py
@@ -8,6 +8,8 @@ from database import load_movies
 from bot import send_message
 from config import BOT_TOKEN
 from webhook import log_to_discord
+from utils import get_username
+username = get_username(user)
 
 
 def handle_delete_movie(chat_id, text, user_id, pending_delete, user):
@@ -49,7 +51,6 @@ def handle_delete_movie(chat_id, text, user_id, pending_delete, user):
         }
     )
 
-    username = f"@{user['username']}" if user.get("username") else user.get("first_name", "Admin")
 
     log_to_discord(
         message="🗑 Delete Requested",
