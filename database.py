019c6f58ffaaa27934f0e90a57bed27b15c88f5b
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-04-27 03:09:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import secrets
 import string
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure, DuplicateKeyError
-from config import MONGODB_URI, ADMIN_IDS
+from config import MONGODB_URI
 from webhook import log_to_discord
 from redis_client import get_cache, set_cache, delete_cache, REDIS_AVAILABLE
 
