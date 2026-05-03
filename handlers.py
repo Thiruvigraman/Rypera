4dd03bfd9f1d8d8cd93683a553e64a29b242a495
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-05-03 10:43:52 +0530

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -18,6 +18,9 @@ from commands.list_movies import (
     handle_list_movies,
     send_page
 )
+from commands.migrate_metadata import (
+    handle_migrate_metadata
+)
 from commands.list_groups import (
     handle_list_groups
 )
