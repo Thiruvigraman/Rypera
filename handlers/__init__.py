479bbe4a3fc78444659f6ed70779e8427e86178f
Thiruvigraman <165885391+Thiruvigraman@users.noreply.github.com>
2026-05-03 16:45:07 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,24 +1,3 @@
 # file: handlers/__init__.py
 
-import importlib.util
-import os
-
-
-current_dir = os.path.dirname(__file__)
-
-root_handlers_path = os.path.join(
-    current_dir,
-    "..",
-    "main_handler.py"
-)
-
-spec = importlib.util.spec_from_file_location(
-    "root_handlers",
-    root_handlers_path
-)
-
-root_handlers = importlib.util.module_from_spec(spec)
-
-spec.loader.exec_module(root_handlers)
-
-process_update = root_handlers.process_update
+from main_handler import process_update
